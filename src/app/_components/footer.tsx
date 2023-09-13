123f22f6002f860c6ab8e4ef8470356a264a77eb
Rakesh Kalyankar <k.rakeshlal@gmail.com>
2023-09-13 21:37:48 +0530

    first commit

---
+++ src/app/_components/footer.tsx
@@ -0,0 +1,7 @@
+export const Footer = () => (
+  <footer className="mt-auto py-3 bg-gray-800">
+    <div className="max-w-[85rem] mx-auto px-4 sm:px-6 lg:px-8">
+      <p className="text-white">© Prestige Constructions - 2023</p>
+    </div>
+  </footer>
+)
---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main id="content" role="main" className="shrink-0">
+      <div className="max-w-[85rem] mx-auto py-10 px-4 sm:px-6 lg:px-8">
+
+      </div>
+    </main>
+  )
+}
